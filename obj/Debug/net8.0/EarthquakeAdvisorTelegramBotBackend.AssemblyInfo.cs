6a044ead1f3da4fc5d9fcbca158b2cccfb3ef263
marcopnzz <marcopen21@gmail.com>
2025-02-21 00:26:50 +0100

    Ignore inserito

--- obj/Debug/net8.0/EarthquakeAdvisorTelegramBotBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/EarthquakeAdvisorTelegramBotBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EarthquakeAdvisorTelegramBotBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+417247e771afbdb9368a6beb9199c2711ac2d4e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3df62533455d254c145446a10989633e73984f83")]
 [assembly: System.Reflection.AssemblyProductAttribute("EarthquakeAdvisorTelegramBotBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EarthquakeAdvisorTelegramBotBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
